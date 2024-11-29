503f7e67bd59a060f1e86aa3b48b5f21b2e67b4e
Ashish Gupta <126087758+Ashish9720@users.noreply.github.com>
2024-11-29 11:45:58 +0530

    Update shell.c

--- shell.c
+++ shell.c
@@ -31,19 +31,20 @@ removes the newline and space character from the end and start of a char*
 void removeWhiteSpace(char* buf) {
     if (buf[strlen(buf) - 1] == ' ' || buf[strlen(buf) - 1] == '\n')
         buf[strlen(buf) - 1] = '\0';
-    if (buf[0] == ' ' || buf[0] == '\n') memmove(buf, buf + 1, strlen(buf));
+    if (buf[0] == ' ' || buf[0] == '\n')
+        memmove(buf, buf + 1, strlen(buf));
 }
 
 /*
 tokenizes char* buf using the delimiter c, and returns the array of strings in param
 and the size of the array in pointer nr
 */
-void tokenize_buffer(char** param, int* nr, char* buf, const char* c) {
-    char* token;
+void tokenize_buffer(char** param, int *nr, char *buf, const char *c) {
+    char *token;
     token = strtok(buf, c);
     int pc = -1;
     while (token) {
-        param[++pc] = malloc(sizeof(token) + 1);
+        param[++pc] = malloc(strlen(token) + 1);
         strcpy(param[pc], token);
         removeWhiteSpace(param[pc]);
         token = strtok(NULL, c);
@@ -55,7 +56,7 @@ void tokenize_buffer(char** param, int* nr, char* buf, const char* c) {
 /*
 used for debugging purposes to print all the strings in a string array
 */
-void print_params(char** param) {
+void print_params(char **param) {
     while (*param) {
         printf("param=%s..\n", *param++);
     }
@@ -75,13 +76,13 @@ void executeBasic(char** argv) {
     }
 
     if (fork() > 0) {
-        // parent
+        // Parent
         wait(NULL);
     } else {
-        // child
+        // Child
         execvp(argv[0], argv);
-        // in case exec is not successful, exit
-        perror(ANSI_COLOR_RED "invalid input" ANSI_COLOR_RESET "\n");
+        // In case exec is not successful, exit
+        perror(ANSI_COLOR_RED "Invalid input" ANSI_COLOR_RESET "\n");
         exit(1);
     }
 }
@@ -89,11 +90,11 @@ void executeBasic(char** argv) {
 /*
 loads and executes a series of external commands that are piped together
 */
-void executePiped(char** buf, int nr) { // can support up to 10 piped commands
+void executePiped(char** buf, int nr) { // Can support up to 10 piped commands
     if (nr > 10) return;
 
     int fd[10][2], i, pc;
-    char* argv[100];
+    char *argv[100];
 
     for (i = 0; i < nr; i++) {
         tokenize_buffer(argv, &pc, buf[i], " ");
@@ -108,11 +109,11 @@ void executePiped(char** buf, int nr) { // can support up to 10 piped commands
 
         if (i != nr - 1) {
             if (pipe(fd[i]) < 0) {
-                perror("pipe creating was not successful\n");
+                perror("Pipe creation failed\n");
                 return;
             }
         }
-        if (fork() == 0) { // child1
+        if (fork() == 0) { // Child
             if (i != nr - 1) {
                 dup2(fd[i][1], 1);
                 close(fd[i][0]);
@@ -125,11 +126,11 @@ void executePiped(char** buf, int nr) { // can support up to 10 piped commands
                 close(fd[i - 1][0]);
             }
             execvp(argv[0], argv);
-            perror("invalid input ");
-            exit(1); // in case exec is not successful, exit
+            perror("Invalid input");
+            exit(1);
         }
-        // parent
-        if (i != 0) { // second process
+        // Parent
+        if (i != 0) { // Close previous pipe
             close(fd[i - 1][0]);
             close(fd[i - 1][1]);
         }
@@ -138,51 +139,116 @@ void executePiped(char** buf, int nr) { // can support up to 10 piped commands
 }
 
 /*
+loads and executes a series of external commands that have to be run asynchronously
+*/
+void executeAsync(char** buf, int nr) {
+    int i, pc;
+    char *argv[100];
+    for (i = 0; i < nr; i++) {
+        tokenize_buffer(argv, &pc, buf[i], " ");
+        if (fork() == 0) {
+            execvp(argv[0], argv);
+            perror("Invalid input");
+            exit(1);
+        }
+    }
+    for (i = 0; i < nr; i++) {
+        wait(NULL);
+    }
+}
+
+/*
+loads and executes an external command that needs file redirection(input, output or append)
+*/
+void executeRedirect(char** buf, int nr, int mode) {
+    int pc, fd;
+    char *argv[100];
+    removeWhiteSpace(buf[1]);
+    tokenize_buffer(argv, &pc, buf[0], " ");
+    if (fork() == 0) {
+
+        switch (mode) {
+        case INPUT:  fd = open(buf[1], O_RDONLY); break;
+        case OUTPUT: fd = open(buf[1], O_WRONLY | O_CREAT | O_TRUNC, 0644); break;
+        case APPEND: fd = open(buf[1], O_WRONLY | O_CREAT | O_APPEND, 0644); break;
+        default: return;
+        }
+
+        if (fd < 0) {
+            perror("Cannot open file\n");
+            return;
+        }
+
+        switch (mode) {
+        case INPUT:   dup2(fd, 0); break;
+        case OUTPUT:  dup2(fd, 1); break;
+        case APPEND:  dup2(fd, 1); break;
+        default: return;
+        }
+        execvp(argv[0], argv);
+        perror("Invalid input");
+        exit(1); // In case exec is not successful, exit
+    }
+    wait(NULL);
+}
+
+/*
 shows the internal help
 */
 void showHelp() {
     printf(ANSI_COLOR_GREEN "----------Help--------" ANSI_COLOR_RESET "\n");
-    printf(ANSI_COLOR_GREEN "Supported commands: cd, pwd, sa (ls), noise (echo), exit" ANSI_COLOR_RESET "\n");
-    printf(ANSI_COLOR_GREEN "Commands can be piped together (e.g., sa -a | noise hello)" ANSI_COLOR_RESET "\n");
-    printf(ANSI_COLOR_GREEN "Input and output redirection are supported (e.g., sa > file, noise < file)" ANSI_COLOR_RESET "\n");
+    printf(ANSI_COLOR_GREEN "Supported commands: sa (ls), noise (echo), cd, pwd, exit" ANSI_COLOR_RESET "\n");
+    printf(ANSI_COLOR_GREEN "Piping: Ex. sa | noise \"Hello\"" ANSI_COLOR_RESET "\n");
+    printf(ANSI_COLOR_GREEN "Asynchronous: Ex. sa & noise \"Done!\"" ANSI_COLOR_RESET "\n");
+    printf(ANSI_COLOR_GREEN "Input/output redirection: Ex. sa > file, sa >> file, noise < file" ANSI_COLOR_RESET "\n");
 }
 
-int main(int argc, char** argv) {
-    char buf[500], *buffer[100], *params1[100], cwd[1024];
+int main() {
+    char buf[500], *buffer[100], cwd[1024];
     int nr = 0;
-
-    printf(ANSI_COLOR_GREEN "*****************************************************************" ANSI_COLOR_RESET "\n");
     printf(ANSI_COLOR_GREEN "**************************CUSTOM SHELL***************************" ANSI_COLOR_RESET "\n");
 
     while (1) {
-        printf(ANSI_COLOR_BLUE "Enter command (or 'exit' to exit):" ANSI_COLOR_RESET "\n");
-
-        // print current directory
+        printf(ANSI_COLOR_BLUE "Enter command (or 'exit' to quit):" ANSI_COLOR_RESET "\n");
         if (getcwd(cwd, sizeof(cwd)) != NULL)
-            printf(ANSI_COLOR_GREEN "%s  " ANSI_COLOR_RESET, cwd);
+            printf(ANSI_COLOR_GREEN "%s $ " ANSI_COLOR_RESET, cwd);
         else
             perror("getcwd failed\n");
 
-        // read user input
-        fgets(buf, 500, stdin); // buffer overflow cannot happen
+        fgets(buf, 500, stdin);
+        buf[strcspn(buf, "\n")] = 0; // Remove newline
 
-        // check if only a simple command needs to be executed or multiple piped commands or other types
-        if (strchr(buf, '|')) { // tokenize pipe commands
+        if (strchr(buf, '|')) {
             tokenize_buffer(buffer, &nr, buf, "|");
             executePiped(buffer, nr);
-        } else { // single command including internal ones
-            tokenize_buffer(params1, &nr, buf, " ");
-            if (strcmp(params1[0], "cd") == 0) { // cd builtin command
-                chdir(params1[1]);
-            } else if (strcmp(params1[0], "help") == 0) { // help builtin command
+        } else if (strchr(buf, '&')) {
+            tokenize_buffer(buffer, &nr, buf, "&");
+            executeAsync(buffer, nr);
+        } else if (strstr(buf, ">>")) {
+            tokenize_buffer(buffer, &nr, buf, ">>");
+            if (nr == 2) executeRedirect(buffer, nr, APPEND);
+            else printf("Incorrect redirection format\n");
+        } else if (strchr(buf, '>')) {
+            tokenize_buffer(buffer, &nr, buf, ">");
+            if (nr == 2) executeRedirect(buffer, nr, OUTPUT);
+            else printf("Incorrect redirection format\n");
+        } else if (strchr(buf, '<')) {
+            tokenize_buffer(buffer, &nr, buf, "<");
+            if (nr == 2) executeRedirect(buffer, nr, INPUT);
+            else printf("Incorrect redirection format\n");
+        } else {
+            char *params[100];
+            tokenize_buffer(params, &nr, buf, " ");
+            if (strcmp(params[0], "cd") == 0) {
+                if (chdir(params[1]) < 0) perror("cd failed");
+            } else if (strcmp(params[0], "help") == 0) {
                 showHelp();
-            } else if (strcmp(params1[0], "exit") == 0) { // exit builtin command
+            } else if (strcmp(params[0], "exit") == 0) {
+                printf(ANSI_COLOR_GREEN "Goodbye!" ANSI_COLOR_RESET "\n");
                 exit(0);
             } else {
-                executeBasic(params1);
+                executeBasic(params);
             }
         }
     }
-
-    return 0;
 }
